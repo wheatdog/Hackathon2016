adc565cf11511dceab79eb29171e763134ab2d6e
wheatdog <wheatdoge@gamil.com>
2016-08-21 00:27:30 +0800

    rename to game server

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,72 +1,18 @@
 ﻿using UnityEngine;
-using UnityEngine.Networking;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-	public bool isAtStartup = true;
 
-	NetworkClient myClient;
+	public int player_count;
+	public RemoteInput[] new_inputs, old_inputs;
 
-	// Create a server and listen on a port
-	public void SetupServer()
-	{
-		NetworkServer.Listen(8000);
-		Debug.Log ("Setup server");
-		isAtStartup = false;
+	void Start () {
+		new_inputs = new RemoteInput[player_count];
+		old_inputs = new RemoteInput[player_count];
 	}
 
-	// Create a client and connect to the server port
-	public void SetupClient()
-	{
-		myClient = new NetworkClient();
-		myClient.RegisterHandler(MsgType.Connect, OnConnected);
-		myClient.Connect("127.0.0.1", 4444);
-		isAtStartup = false;
-	}
-
-	// client function
-	public void OnConnected(NetworkMessage netMsg)
-	{
-		Debug.Log("Connected to server");
-	}
-
-	// Create a local client and connect to the local server
-	public void SetupLocalClient()
-	{
-		myClient = ClientScene.ConnectLocalServer();
-		myClient.RegisterHandler(MsgType.Connect, OnConnected);
-		isAtStartup = false;
-	}
-
-	void Update ()
-	{
-		if (isAtStartup)
-		{
-			if (Input.GetKeyDown(KeyCode.S))
-			{
-				SetupServer();
-			}
-
-			if (Input.GetKeyDown(KeyCode.C))
-			{
-				SetupClient();
-			}
-
-			if (Input.GetKeyDown(KeyCode.B))
-			{
-				SetupServer();
-				SetupLocalClient();
-			}
-		}
-	}
-
-	void OnGUI()
-	{
-		if (isAtStartup)
-		{
-			GUI.Label(new Rect(2, 10, 150, 100), "Press S for server");
-			GUI.Label(new Rect(2, 30, 150, 100), "Press B for both");
-			GUI.Label(new Rect(2, 50, 150, 100), "Press C for client");
-		}
+	void Update () {
+		// handle player movement
+	
 	}
 }
