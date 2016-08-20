3936b1089e7d7269e1d7ca28224b79d212fde86f
gitqwerty777 <goohcl777@gmail.com>
2016-08-20 22:58:15 +0800

    add map scene structure

---
+++ Assets/Scripts/initMap.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class initMap : MonoBehaviour {
+
+	PhotonPlayer[] players;
+	bool isGameEnd;
+	public int time = 100;
+//	public List<MapEvent> mapEvents = new List<MapEvent>();
+
+	// Use this for initialization
+	void Start () {
+		initGameSettings();//settings
+		//initMapData();//game logics
+		initPlayers();//healthbar
+		startGame ();//TODO: time
+	}
+
+	void initGameSettings(){
+		//
+	}
+
+	void initMapData(){
+	//	mapEvents.Add(new MapEvent(gameObject, loseHealth));
+	}
+	/*class MapEvent{
+		public GameObject gameObject;
+		public Action<GameObject> reactMethod; 
+		public void MapEvent(GameObject gameObject, Action<GameObject> reactMethod){
+			this.gameObject = gameObject;
+			this.reactMethod = reactMethod;
+		}
+		public void runEvent(){
+			reactMethod (gameObject);
+		}
+	}
+
+	void loseHealth(GameObject gameObject){
+		PlayerManger playerManager = (PlayerManager)gameObject;
+		playerManager.changeHealth (-10);
+	}*/
+
+	void initPlayers(){
+
+	}
+
+	void startGame(){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {//handle game logics
+		//if (timeUp ()) {
+		//
+		//}
+
+		//move players
+		//check interaction between map and player
+		//update and show player stats
+
+	}
+}
---
+++ Assets/Scripts/playerManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerManager : MonoBehaviour {
+
+	public int health = 100;
+	public string role = "None";
+	public string item = "None";
+	public int INT = 10;
+	public int moveSpeed = 10;
+	public int damage = 10;
+	public UnityEngine.UI.Image healthBar;
+
+	public void PlayerManager(){
+
+	}
+
+	public void changeHealth(int value){
+		health -= value;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//healthBar.
+	}
+
+
+}
